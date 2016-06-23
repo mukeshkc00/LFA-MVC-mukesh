8d69caf72f210574c9faaadd069b8b21268c6e38
mukeshkc0123@gmail.com <mukeshkhatri>
2016-06-23 11:13:40 +0545

    login console

---
+++ loginauthentication/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace loginauthentication
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            String username;
+            String password;
+            String[,] accnts = { { "mukes", "dada" }, { "admin", "fafa" }, { "admin", "gaga" } };
+            int row;
+            bool isValideUser = false;
+            for (int x = 3; x >= 1; x--)
+            {
+                Console.WriteLine("You have " + x + " attempt/s.");
+                Console.Write("Enter Username>> ");
+                username = Console.ReadLine();
+                Console.Write("Enter Password>> ");
+                password = Console.ReadLine();
+                for (row = 0; row < 3; row++)
+                {
+                    if (username.Equals(accnts[row, 0]) && password.Equals(accnts[row, 1]))
+                    {
+                        Console.WriteLine("Welcome " + accnts[row, 0] + "!");
+                        isValideUser = true;
+                        break;
+                    }
+                }
+                if (!isValideUser)
+                {
+                    Console.WriteLine("Invalid Input.");
+                    if (x != 1)
+                    {
+                        Console.WriteLine("Please Try Again.");
+                        Console.Write("\n");
+                    }
+                    else if (x.Equals(1))
+                    {
+                        Console.Write("Goodbye!");
+                        break;
+                    }
+                }
+                else
+                {
+                    break;
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
